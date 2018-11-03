351880b9eab60aa419d50bc39b3d66bd78252e02
Aviral kumar <aviralkumar@Avirals-MacBook-Pro.local>
2018-11-03 01:45:57 -0700

    Adding the Base Test Class which other Test Classes will extend

---
+++ AutomationFrameworkClasses/src/main/java/automation/framework/core/BaseTest.java
@@ -0,0 +1,12 @@
+package automation.framework.core;
+
+
+/**
+ * BaseTest class is extended by all Test Classes and it provides the Webdriver that can be used by other class
+ * WebDriver Intialization happnes here so that the test classes can use the webdriver instance
+ * The Test Classes will simply get the Webdriver method by calling the getDriver() method of the class
+ */
+public class BaseTest {
+
+
+}
