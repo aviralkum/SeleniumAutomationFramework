5d4fa80b1810833314ddac479b0b73cdb2370406
Aviral kumar <aviralkumar@Avirals-MacBook-Pro.local>
2018-11-03 04:29:06 -0700

    Changes in the Base Test Class

--- AutomationFrameworkClasses/src/main/java/automation/framework/core/BaseTest.java
+++ AutomationFrameworkClasses/src/main/java/automation/framework/core/BaseTest.java
@@ -6,7 +6,46 @@ package automation.framework.core;
  * WebDriver Intialization happnes here so that the test classes can use the webdriver instance
  * The Test Classes will simply get the Webdriver method by calling the getDriver() method of the class
  */
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
 public class BaseTest {
 
+    private WebDriver driver;
 
+    @BeforeSuite
+    public void beforeSuite() {
+        System.setProperty("headless", "false"); // You can set this property elsewhere
+        String headless = System.getProperty("headless");
+
+        ChromeDriverManager.chromedriver();
+        if("true".equals(headless)) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
+        } else {
+            driver = new ChromeDriver();
+        }
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        if(null != driver) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
+
+
+
