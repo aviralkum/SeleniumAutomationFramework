4e783595cf9414b1dc649a1f3375960fe9fc5d9e
Aviral kumar <aviralkumar@Avirals-MacBook-Pro.local>
2018-11-04 21:35:07 -0800

    test class added

---
+++ AutomationFrameworkClasses/src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+
+import org.testng.*;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class TestClass {
+
+
+    @Test
+    public void simpleTest(){
+
+       Assert.assertEquals(2,2);
+    }
+}
